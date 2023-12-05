7128273f092937ef152413cbf99df3ff835d7979
Invitado <invitado@s2pc27.eisc.univalle.edu.co>
2023-12-05 18:12:16 -0500

    HU2. Avances en los input

--- app/src/main/java/com/cristian/miniproyecto2/fragments/auth.kt
+++ app/src/main/java/com/cristian/miniproyecto2/fragments/auth.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
 import com.cristian.miniproyecto2.R
 import com.cristian.miniproyecto2.databinding.FragmentAuthBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class auth : Fragment() {
     private lateinit var binding: FragmentAuthBinding
@@ -16,6 +19,25 @@ class auth : Fragment() {
     ): View? {
         binding = FragmentAuthBinding.inflate(inflater)
         binding.lifecycleOwner = this
+
+        binding.password.doOnTextChanged{ text,start,before,count ->
+            if(text?.length?: 0 < 6){
+                binding.password.error = "Mínimo 6 dígitos"
+                binding.passwordInputLayout.endIconMode = TextInputLayout.END_ICON_NONE
+                binding.passwordInputLayout.boxStrokeColor = ContextCompat.getColor(requireContext(), android.R.color.holo_red_dark)
+                binding.passwordInputLayout.hintTextColor = ContextCompat.getColorStateList(requireContext(), android.R.color.holo_red_dark)
+
+            }
+            else{
+                binding.password.error = null
+                binding.passwordInputLayout.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+                binding.passwordInputLayout.boxStrokeColor = ContextCompat.getColor(requireContext(), R.color.white)
+                binding.passwordInputLayout.hintTextColor = ContextCompat.getColorStateList(requireContext(), R.color.white)
+            }
+
+        }
+
         return binding.root
     }
+
 }
