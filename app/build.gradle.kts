7da55ef0cbe51695528c62f66ab424417fdab3d2
Santiagospitia <ospitia2002@gmail.com>
2023-12-08 17:58:18 -0500

    frontend hu5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,7 @@ dependencies {
     implementation ("androidx.activity:activity-ktx:1.8.0")
     implementation ("androidx.fragment:fragment-ktx:1.6.2")
 
+    //Cardview
+    implementation ("androidx.cardview:cardview:1.0.0")
+
 }
