992644f54b048f5dab5ff64841a1113b903f9c07
Laura Moyano <laura.moyano.go@gmail.com>
2023-12-10 02:41:39 -0500

    layout add product button add

--- app/src/main/java/com/cristian/miniproyecto2/view/fragments/AddProduct.kt
+++ app/src/main/java/com/cristian/miniproyecto2/view/fragments/AddProduct.kt
@@ -1,56 +1,56 @@
 package com.cristian.miniproyecto2.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.cristian.miniproyecto2.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
+import com.google.android.material.textfield.TextInputEditText
+import androidx.fragment.app.Fragment
 
 class AddProduct : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var codigoEditText: TextInputEditText
+    private lateinit var nombreEditText: TextInputEditText
+    private lateinit var precioEditText: TextInputEditText
+    private lateinit var cantidadEditText: TextInputEditText
+    private lateinit var guardarButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_product, container, false)
+        val view = inflater.inflate(R.layout.fragment_add_product, container, false)
+
+        // Obtener referencias a los elementos de la interfaz de usuario
+        codigoEditText = view.findViewById(R.id.codigoEditText)
+        nombreEditText = view.findViewById(R.id.nombreEditText)
+        precioEditText = view.findViewById(R.id.precioEditText)
+        cantidadEditText = view.findViewById(R.id.cantidadEditText)
+        guardarButton = view.findViewById(R.id.guardarButton)
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddProduct.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddProduct().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Configurar el evento de clic del botón "Guardar"
+        guardarButton.setOnClickListener {
+            // Acciones que se realizarán al hacer clic en el botón "Guardar"
+            guardarProducto()
+        }
+    }
+
+    private fun guardarProducto() {
+        // Obtener los valores ingresados por el usuario
+        val codigo = codigoEditText.text.toString()
+        val nombre = nombreEditText.text.toString()
+        val precio = precioEditText.text.toString()
+        val cantidad = cantidadEditText.text.toString()
+
+        // Realizar acciones para guardar el producto en la base de datos o donde sea necesario
+        // Puedes utilizar la lógica de tu función guardarArticulo() aquí
     }
 }
