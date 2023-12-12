6c0b9bc50831b055c2065c23734450e293e0017f
Sebastian Caicedo <sdrivert@hotmail.com>
2023-12-11 21:54:04 -0500

    Add inventario repository

--- app/src/main/java/com/cristian/miniproyecto2/viewmodel/InventarioViewModel.kt
+++ app/src/main/java/com/cristian/miniproyecto2/viewmodel/InventarioViewModel.kt
@@ -2,39 +2,18 @@ package com.cristian.miniproyecto2.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.cristian.miniproyecto2.model.Articulo
+import com.cristian.miniproyecto2.repository.InventarioRepository
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlin.math.log
 
 class InventarioViewModel : ViewModel() {
     private val db = FirebaseFirestore.getInstance()
+    private val repository = InventarioRepository()
 
     fun guardarArticulo(articulo: Articulo) {
-        db.collection("articulo").document(articulo.id.toString()).set(
-            hashMapOf(
-                "id" to articulo.id,
-                "name" to articulo.name,
-                "price" to articulo.price,
-                "quantity" to articulo.quantity
-            )
-        )
+        repository.guardarArticulo(articulo)
     }
 
-
     fun listarArticulos(): MutableList<Articulo> {
-        var articulos = mutableListOf<Articulo>()
-        var suma = 0L
-        db.collection("articulo").get().addOnSuccessListener {
-            for (document in it.documents) {
-                val articulo = Articulo( // crea un objeto Articulo con los datos del documento
-                    id = document.get("id") as Long,
-                    name = document.get("name") as String,
-                    price = document.get("price") as Long,
-                    quantity = document.get("quantity") as Long
-                )
-                articulos.add(articulo) // añade el objeto Articulo a la lista articulos
-                suma += articulo.price * articulo.quantity
-            }
-        }
-        return articulos
+        return repository.listarArticulos()
     }
 }
